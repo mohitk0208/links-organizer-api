e63ec9235e0c708fef12172a5c3f464dc63c691e
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-10-31 15:41:47 +0530

    made login urls and views
    also endpoints to refresh and verify tokens

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,14 @@
-# from django.urls import path
+from django.urls import path
 
-urlpatterns = []
+from .views import (
+  DecoratedTokenObtainPairView,
+  DecoratedTokenRefreshView,
+  DecoratedTokenVerifyView,
+)
+
+urlpatterns = [
+  # JWT
+  path("login", DecoratedTokenObtainPairView.as_view(), name="login"),
+  path("login/refresh", DecoratedTokenRefreshView.as_view(), name="token_refresh"),
+  path("login/verify", DecoratedTokenVerifyView.as_view(), name="token_verify"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+# from django.conf import settings
+from django.contrib.auth import get_user_model
+# from django.contrib.auth.tokens import PasswordResetTokenGenerator
+# from django.core.validators import EmailValidator
+# from django.shortcuts import get_object_or_404
+# from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+# from rest_framework import status
+# from rest_framework.exceptions import APIException, ParseError
+# from rest_framework.permissions import AllowAny
+# from rest_framework.request import Request
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from rest_framework_simplejwt.token_blacklist.models import (
+#     BlacklistedToken,
+#     OutstandingToken,
+# )
+# from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from .models import User
+from .serializers import (
+    # UserSerializer,
+    # PublicUserSerializer,
+    TokenObtainPairResponseSerializer,
+    TokenRefreshResponseSerializer,
+    TokenVerifyResponseSerializer,
+)
+
+# from .validators import UsernameValidator
+
+
+UserModel: User = get_user_model()
 
 # Create your views here.
+
+
+class DecoratedTokenObtainPairView(TokenObtainPairView):
+    @swagger_auto_schema(
+        tags=("auth",),
+        security=[],
+        operation_summary="Obtain token pair",
+        responses={
+            200: TokenObtainPairResponseSerializer,
+            400: "Bad request",
+            401: "Unauthorized",
+        },
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class DecoratedTokenRefreshView(TokenRefreshView):
+    @swagger_auto_schema(
+        tags=("auth",),
+        security=[],
+        operation_id="accounts_login_refresh",
+        operation_summary="Refresh access token",
+        responses={
+            200: TokenRefreshResponseSerializer,
+            400: "Bad request",
+            401: "Unauthorized",
+        },
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class DecoratedTokenVerifyView(TokenVerifyView):
+    @swagger_auto_schema(
+        tags=("auth",),
+        security=[],
+        operation_id="accounts_login_verify",
+        operation_summary="Verify token",
+        responses={
+            200: TokenVerifyResponseSerializer,
+            400: "Bad request",
+            401: "Unauthorized",
+        },
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
