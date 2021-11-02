990c5b51eed8c73db6054a4cc9671462eb8e444f
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-11-02 17:28:41 +0530

    made IsOwner permission class

--- links_organizer_api/utils/permissions.py
+++ links_organizer_api/utils/permissions.py
@@ -14,3 +14,13 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.owner == request.user
