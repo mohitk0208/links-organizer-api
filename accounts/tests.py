747eef25ef261b81fd041016ec1e5672b783850d
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-04-04 17:12:41 +0530

    test cases for accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,123 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+from accounts.models import User
+from accounts.views import (
+    DecoratedTokenObtainPairView,
+    UserProfileView,
+    UserRegisterView,
+)
+
+User: User = get_user_model()
+
+
+class AccountRegisterApiTests(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    def test_user_can_create_account(self):
+        request = self.factory.post(
+            "/api/accounts/users",
+            {
+                "username": "test",
+                "password": "test",
+                "email": "test@test.com",
+                "first_name": "test",
+            },
+            format="json",
+        )
+        response = UserRegisterView.as_view()(request)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue("refresh" in response.data)
+        self.assertTrue("access" in response.data)
+        get_user = User.objects.get(username="test")
+        self.assertEqual(get_user.avatar, User.generate_avatar("test@test.com"))
+        self.assertEqual(str(get_user), "test")
+
+
+class AccountsApiTests(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(
+            username="test", password="test", email="test@test.com", first_name="test"
+        )
+
+    # def test_user_exists(self):
+    #     request = self.factory.post(
+    #         "/api/accounts/user/exists",
+    #         {
+    #             "username": "test",
+    #             "password": "test",
+    #             "email": "
+
+    def test_user_can_login(self):
+        request = self.factory.post(
+            "/api/accounts/login",
+            {"username": "test", "password": "test"},
+            format="json",
+        )
+        view = DecoratedTokenObtainPairView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("refresh" in response.data)
+        self.assertTrue("access" in response.data)
+
+    def test_user_get_profile_view(self):
+        request = self.factory.get("/api/accounts/user/profile", format="json")
+        force_authenticate(request, user=self.user)
+        response = UserProfileView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["username"], self.user.username)
+        self.assertEqual(response.data["email"], self.user.email)
+        self.assertEqual(response.data["first_name"], self.user.first_name)
+        self.assertEqual(response.data["last_name"], self.user.last_name)
+        self.assertEqual(response.data["avatar"], self.user.avatar)
+
+
+class ProfileUpdateTests(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(
+            username="test", password="test", email="test@test.com", first_name="test"
+        )
+
+    def make_request(self, data):
+        request = self.factory.patch("/api/accounts/user/profile", data, format="json")
+        force_authenticate(request, user=self.user)
+        return UserProfileView.as_view()(request)
+
+    def test_user_can_change_avatar(self):
+        """
+        test user can change avatar
+        """
+        response = self.make_request({"avatar": User.generate_avatar("something")})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["avatar"], User.generate_avatar("something"))
+
+    def test_user_can_change_username(self):
+        response = self.make_request({"username": "test1"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["username"], "test1")
+
+    def test_user_can_change_email(self):
+        response = self.make_request({"email": "test1@test.com"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["email"], "test1@test.com")
+
+    def test_user_can_change_first_name(self):
+        response = self.make_request({"first_name": "test1"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["first_name"], "test1")
+
+    def test_user_can_change_last_name(self):
+        response = self.make_request({"last_name": "test1"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["last_name"], "test1")
+
+    def test_user_can_change_password(self):
+        response = self.make_request({"password": "test1"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(self.user.check_password("test1"))
