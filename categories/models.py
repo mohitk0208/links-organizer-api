df83d4e0bee84509ca9d5d6f562a1df62615cbee
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-05-11 15:35:08 +0530

    added models for shared categories and user permissions

--- categories/models.py
+++ categories/models.py
@@ -1,6 +1,12 @@
 from django.conf import settings
-from django.db import models
 from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class AccessLevel(models.IntegerChoices):
+    ADMIN = 100
+    READ_WRITE = 200
+    READ_ONLY = 300
 
 
 class Category(models.Model):
@@ -13,6 +19,12 @@ class Category(models.Model):
     parent_category = models.ForeignKey(
         'self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories'
         )
+    shared_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through='categories.CategoryAccess',
+        through_fields=('category', 'user'),
+        related_name='shared_categories'
+    )
 
     class Meta:
         verbose_name_plural = 'categories'
@@ -25,5 +37,34 @@ class Category(models.Model):
             raise ValidationError('Category cannot be its own parent')
         return super(Category, self).save(*args, **kwargs)
 
+
+    def get_access_level(self, user_id: int):
+        try:
+            access = self.access.through.objects.get(category_id=self.id, user_id=user_id)
+        except CategoryAccess.DoesNotExist:
+            return None
+        return access.level
+
+
+    def get_category(self):
+        return self
+
+
     def __str__(self) -> str:
         return self.name
+
+
+
+class CategoryAccess(models.Model):
+    category = models.ForeignKey(Category,db_index=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    level = models.IntegerField(choices=AccessLevel.choices, default=AccessLevel.READ_ONLY)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['category', 'user'], name='unique_category_access_through')
+        ]
+
+
+
--- links_organizer_api/utils/permissions.py
+++ links_organizer_api/utils/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+from categories.models import AccessLevel
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
@@ -24,3 +26,33 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
+
+
+
+class CategoryAccessPermission(permissions.IsAuthenticated):
+    def __init__(
+        self,
+        read_level=AccessLevel.READ_ONLY,
+        write_level=AccessLevel.READ_WRITE,
+    ):
+        super().__init__()
+        self.read_level = read_level
+        self.write_level = write_level
+
+    def has_object_permission(self, request, _, obj):
+
+        category = obj.get_category()
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        access = category.get_access_level(request.user.id)
+
+        if access is None:
+            return False
+
+        return (
+            access <= self.read_level
+            if request.method in permissions.SAFE_METHODS
+            else access <= self.write_level
+        )
