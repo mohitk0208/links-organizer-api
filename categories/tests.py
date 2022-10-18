0be9862db3901230b6fdfc8dd289d93824a4ad2d
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-10-18 15:09:42 +0530

    completed test cases for crud operations on categories

--- categories/tests.py
+++ categories/tests.py
@@ -41,6 +41,50 @@ class CategoryCreateApiTests(TestCase):
         self.assertEqual(response.data["owner_avatar"], self.user.avatar)
         self.assertEqual(response.data["shared_users"], [])
 
+    def test_same_category_name_same_owner(self):
+        request_data = {
+            "name": "category1",
+            "description": "This is a test category.",
+            "background_url": "https://picsum.photos/400"
+        }
+        category1 = Category.objects.create(name=request_data["name"], description=request_data["description"], background_url=request_data["background_url"], owner=self.user)
+
+        request = self.factory.post("/api/categories/",request_data,format="json")
+        force_authenticate(request, user=self.user)
+        response = CategoryViewSet.as_view({"post": "create"})(request)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["name"][0], "Category already exists")
+
+
+
+    def test_same_category_name_different_owner(self):
+        request_data = {
+            "name": "category1",
+            "description": "This is a test category.",
+            "background_url": "https://picsum.photos/400"
+        }
+        user2 = User.objects.create_user(
+            username= "test2",
+            password="test2",
+            email="test2@test.com",
+            first_name="test2"
+        )
+
+        category1 = Category.objects.create(name=request_data["name"], description=request_data["description"], background_url=request_data["background_url"], owner=user2)
+
+        request = self.factory.post("/api/categories/",request_data,format="json")
+        force_authenticate(request, user=self.user)
+        response = CategoryViewSet.as_view({"post": "create"})(request)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data["name"], request_data["name"])
+        self.assertEqual(response.data["description"], request_data["description"])
+        self.assertEqual(response.data["background_url"], request_data["background_url"])
+        self.assertEqual(response.data["owner"], self.user.id)
+        self.assertEqual(response.data["owner_username"], self.user.username)
+        self.assertEqual(response.data["owner_avatar"], self.user.avatar)
+        self.assertEqual(response.data["shared_users"], [])
 
 class CategoryListApiTests(TestCase):
     def setUp(self):
@@ -113,3 +157,105 @@ class CategoryListApiTests(TestCase):
         self.assertEqual(response.data["results"][2]["id"], self.category3.id)
 
 
+class CategoryDetailApiTests(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(
+            username= "test",
+            password="test",
+            email="test@test.com",
+            first_name="test"
+        )
+        self.category1 = Category.objects.create(name="Category1", description="some description", background_url="https://example.com/400", owner=self.user)
+        self.category2 = Category.objects.create(name="Category2", description="some description", background_url="https://example.com/400", owner=self.user)
+        self.category3 = Category.objects.create(name="Category3", description="some description", background_url="https://example.com/400", owner=self.user)
+
+
+    def test_get_category_detail(self):
+        request = self.factory.get(f"/api/categories/{self.category1.id}/", format="json")
+        force_authenticate(request, user=self.user)
+        response = CategoryViewSet.as_view({"get":"retrieve"})(request, pk=self.category1.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], self.category1.name)
+        self.assertEqual(response.data["description"], self.category1.description)
+        self.assertEqual(response.data["background_url"], self.category1.background_url)
+        self.assertEqual(response.data["owner"], self.category1.owner.id)
+        self.assertEqual(response.data["owner_username"], self.category1.owner.username)
+        self.assertEqual(response.data["owner_avatar"], self.category1.owner.avatar)
+        self.assertEqual(response.data["shared_users"], [])
+
+    def test_get_category_of_different_user(self):
+        user2 = User.objects.create_user(
+            username= "test2",
+            password="test2",
+            email="test2@test.com",
+            first_name="test2"
+        )
+        category4 = Category.objects.create(name="Category3", description="some description", background_url="https://example.com/400", owner=user2)
+
+        request = self.factory.get(f"/api/categories/{category4.id}/", format="json")
+        force_authenticate(request, user=self.user)
+        response = CategoryViewSet.as_view({"get":"retrieve"})(request, pk=category4.id)
+
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_update_category(self):
+        updated_data = {
+            "name": "new_name",
+            "description": "This is updated description."
+        }
+        request = self.factory.put(f"/api/categories/{self.category1.id}/", updated_data,format="json")
+        force_authenticate(request, user=self.user)
+        response = CategoryViewSet.as_view({"put":"update"})(request, pk=self.category1.id)
+
+        self.category1 = Category.objects.get(id=self.category1.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.category1.name, updated_data["name"])
+        self.assertEqual(response.data["name"], self.category1.name)
+        self.assertEqual(self.category1.description, updated_data["description"])
+        self.assertEqual(response.data["description"], self.category1.description)
+        self.assertEqual(response.data["background_url"], self.category1.background_url)
+        self.assertEqual(response.data["owner"], self.category1.owner.id)
+        self.assertEqual(response.data["owner_username"], self.category1.owner.username)
+        self.assertEqual(response.data["owner_avatar"], self.category1.owner.avatar)
+        self.assertEqual(response.data["shared_users"], [])
+
+
+    def test_partial_update_category(self):
+        updated_data = {
+            "description": "This is updated description."
+        }
+        request = self.factory.patch(f"/api/categories/{self.category1.id}/", updated_data,format="json")
+        force_authenticate(request, user=self.user)
+        response = CategoryViewSet.as_view({"patch":"partial_update"})(request, pk=self.category1.id)
+
+        self.category1 = Category.objects.get(id=self.category1.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.category1.description, updated_data["description"])
+        self.assertEqual(response.data["description"], self.category1.description)
+        self.assertEqual(response.data["name"], self.category1.name)
+        self.assertEqual(response.data["background_url"], self.category1.background_url)
+        self.assertEqual(response.data["owner"], self.category1.owner.id)
+        self.assertEqual(response.data["owner_username"], self.category1.owner.username)
+        self.assertEqual(response.data["owner_avatar"], self.category1.owner.avatar)
+        self.assertEqual(response.data["shared_users"], [])
+
+
+
+    def test_delete_category(self):
+        request = self.factory.delete(f"/api/categories/{self.category1.id}/", format="json")
+        force_authenticate(request, user=self.user)
+        response = CategoryViewSet.as_view({"delete":"destroy"})(request, pk=self.category1.id)
+
+        self.assertEqual(response.status_code, 204)
+        try:
+            self.category1 = Category.objects.get(id=self.category1.id)
+        except Category.DoesNotExist:
+            self.category1 = None
+
+        self.assertEqual(self.category1, None)
